f658ebc70ce2afe0a6fef03bcb5ebe39d39d306d
mmma-dot <urentsev05@mail.ru>
2025-03-12 17:39:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function openEnvelope() {
+    document.querySelector('.flap').style.transform = "rotateX(180deg)";
+    document.querySelector('.letter').style.transform = "translate(-50%, -50%) translateY(0)";
+    document.querySelector('.letter').style.opacity = "1";
+}
